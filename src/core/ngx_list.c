cf3faa31e5f94f26928bd7e6743d2e96ee1e1a52
liqing <liqing.qiya@gmail.com>
2014-10-23 18:12:00 +0800

    ngx_list.c/h的分析实现

--- src/core/ngx_list.c
+++ src/core/ngx_list.c
@@ -14,12 +14,12 @@ ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
     ngx_list_t  *list;
 
-    list = ngx_palloc(pool, sizeof(ngx_list_t));
+    list = ngx_palloc(pool, sizeof(ngx_list_t)); /*创建一个列表结构*/
     if (list == NULL) {
         return NULL;
     }
 
-    if (ngx_list_init(list, pool, n, size) != NGX_OK) {
+    if (ngx_list_init(list, pool, n, size) != NGX_OK) { /*初始化列表, 节点个数为n， 元素大小为size */
         return NULL;
     }
 
@@ -30,34 +30,34 @@ ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
 void *
 ngx_list_push(ngx_list_t *l)
 {
-    void             *elt;
-    ngx_list_part_t  *last;
+    void             *elt; /*元素指针*/
+    ngx_list_part_t  *last; 
 
-    last = l->last;
+    last = l->last; /*存储最后一个节点*/ 
 
-    if (last->nelts == l->nalloc) {
+    if (last->nelts == l->nalloc) { /*链表可用地址没有了,就分配一个新的链表节点 ngx_list_part_t*/
 
         /* the last part is full, allocate a new list part */
 
-        last = ngx_palloc(l->pool, sizeof(ngx_list_part_t));
+        last = ngx_palloc(l->pool, sizeof(ngx_list_part_t)); /*分配一个链表节点*/
         if (last == NULL) {
             return NULL;
         }
 
-        last->elts = ngx_palloc(l->pool, l->nalloc * l->size);
+        last->elts = ngx_palloc(l->pool, l->nalloc * l->size); /*分配每一个节点所需要的存储数据的内存块*/
         if (last->elts == NULL) {
             return NULL;
         }
-
+        /*初始化这个新分配的节点*/
         last->nelts = 0;
         last->next = NULL;
 
         l->last->next = last;
         l->last = last;
     }
+    /*返回可用地址*/
+    elt = (char *) last->elts + l->size * last->nelts; /**/
+    last->nelts++; /*元素个数加一*/
 
-    elt = (char *) last->elts + l->size * last->nelts;
-    last->nelts++;
-
-    return elt;
+    return elt; /*返回可用地址，之后紧接着在这个地址赋值就行了*/
 }
--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -12,22 +12,22 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+/*单链表结构*/
 typedef struct ngx_list_part_s  ngx_list_part_t;
 
 struct ngx_list_part_s {
-    void             *elts;     /*void*类型，？？*/
-    ngx_uint_t        nelts;    /*元素个数？？？*/
-    ngx_list_part_t  *next;     /*指向下一个结构*/
+    void             *elts;     /*指向我们的元素节点*/
+    ngx_uint_t        nelts;    /**/
+    ngx_list_part_t  *next;     /*指向下一个链表元素节点*/
 };
 
 
 typedef struct {
     ngx_list_part_t  *last;     /*指向最后一个节点*/
-    ngx_list_part_t   part;     /*我们的节点*/
-    size_t            size;      /*链表大小*/
-    ngx_uint_t        nalloc;   /*分配的内存大小*/
-    ngx_pool_t       *pool;     /*为什么加上这个？ 内存池*/
+    ngx_list_part_t   part;     /*第一个节点，以组合的方式放在ngx_list_t中*/
+    size_t            size;      /*每个元素内存大小*/
+    ngx_uint_t        nalloc;   /*每一个节点容量大小,也就是节点的个数*/
+    ngx_pool_t       *pool;     /*列表所在内存池*/
 } ngx_list_t;       /*链表结构头*/
 
 
@@ -41,11 +41,11 @@ ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)    
         return NGX_ERROR;
     }
 
-    list->part.nelts = 0;       /*节点个数*/
+    list->part.nelts = 0;       /*一个节点中的元素个数*/
     list->part.next = NULL;     /*part指向的下一个节点*/
     list->last = &list->part;   /*指向最后一个节点*/
-    list->size = size;  /*每一个节点大小*/
-    list->nalloc = n;   /*节点个数*/
+    list->size = size;  /*每一个元素的内存大小*/
+    list->nalloc = n;   /*容量大小，每一个节点可以容下的最大的元素个数*/
     list->pool = pool;  /*内存池*/
 
     return NGX_OK;
