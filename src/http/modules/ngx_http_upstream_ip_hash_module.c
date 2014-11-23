5ce52e11f87253aed5d620dfbfb4a2a03a97e956
liqing <liqing.qiya@gmail.com>
2014-11-23 14:06:17 +0800

    update the date

--- src/http/modules/ngx_http_upstream_ip_hash_module.c
+++ src/http/modules/ngx_http_upstream_ip_hash_module.c
@@ -11,7 +11,7 @@
 
 
 typedef struct {
-    /* the round robin data must be first */
+    /* the round robin data must be first */  /*why??todo*/
     ngx_http_upstream_rr_peer_data_t   rrp;
 
     ngx_uint_t                         hash;
@@ -22,7 +22,7 @@ typedef struct {
     u_char                             tries;
 
     ngx_event_get_peer_pt              get_rr_peer;
-} ngx_http_upstream_ip_hash_peer_data_t;
+} ngx_http_upstream_ip_hash_peer_data_t;            /*todo*/
 
 
 static ngx_int_t ngx_http_upstream_init_ip_hash_peer(ngx_http_request_t *r,
@@ -83,11 +83,11 @@ static u_char ngx_http_upstream_ip_hash_pseudo_addr[3];
 static ngx_int_t
 ngx_http_upstream_init_ip_hash(ngx_conf_t *cf, ngx_http_upstream_srv_conf_t *us)
 {
-    if (ngx_http_upstream_init_round_robin(cf, us) != NGX_OK) { /*初始化加权轮询*/
+    if (ngx_http_upstream_init_round_robin(cf, us) != NGX_OK) {             /*初始化加权轮询*/
         return NGX_ERROR;
     }
 
-    us->peer.init = ngx_http_upstream_init_ip_hash_peer;
+    us->peer.init = ngx_http_upstream_init_ip_hash_peer;                    /*初始化回调函数*/
 
     return NGX_OK;
 }
@@ -95,7 +95,7 @@ ngx_http_upstream_init_ip_hash(ngx_conf_t *cf, ngx_http_upstream_srv_conf_t *us)
 
 static ngx_int_t
 ngx_http_upstream_init_ip_hash_peer(ngx_http_request_t *r,
-    ngx_http_upstream_srv_conf_t *us)
+    ngx_http_upstream_srv_conf_t *us)                                       /*在对应一个request，选择后端服务器之前的初始化函数*/
 {
     struct sockaddr_in                     *sin;
 #if (NGX_HAVE_INET6)
@@ -108,13 +108,13 @@ ngx_http_upstream_init_ip_hash_peer(ngx_http_request_t *r,
         return NGX_ERROR;
     }
 
-    r->upstream->peer.data = &iphp->rrp;        /*非后备服务器列表*/
+    r->upstream->peer.data = &iphp->rrp;                                /*上游服务器列表*/
 
-    if (ngx_http_upstream_init_round_robin_peer(r, us) != NGX_OK) { /*初始化加权轮询模块*/
+    if (ngx_http_upstream_init_round_robin_peer(r, us) != NGX_OK) {   /*初始化加权轮询模块*/
         return NGX_ERROR;
     }
 
-    r->upstream->peer.get = ngx_http_upstream_get_ip_hash_peer; /*设置选服务器的回调函数*/
+    r->upstream->peer.get = ngx_http_upstream_get_ip_hash_peer;       /*设置选服务器的回调函数*/
 
     switch (r->connection->sockaddr->sa_family) {
 
@@ -161,7 +161,7 @@ ngx_http_upstream_get_ip_hash_peer(ngx_peer_connection_t *pc, void *data)
 
     /* TODO: cached */
 
-    if (iphp->tries > 20 || iphp->rrp.peers->single) {
+    if (iphp->tries > 20 || iphp->rrp.peers->single) {  /*当哈系选择失败20次以上，或只有一台后端服务器，此时采用加权轮询策略就可以了*/
         return iphp->get_rr_peer(pc, &iphp->rrp);
     }
 
@@ -174,12 +174,12 @@ ngx_http_upstream_get_ip_hash_peer(ngx_peer_connection_t *pc, void *data)
 
     for ( ;; ) {
 
-        for (i = 0; i < (ngx_uint_t) iphp->addrlen; i++) {
+        for (i = 0; i < (ngx_uint_t) iphp->addrlen; i++) {      /*计算哈系值*/
             hash = (hash * 113 + iphp->addr[i]) % 6271;
         }
 
         if (!iphp->rrp.peers->weighted) {
-            p = hash % iphp->rrp.peers->number;
+            p = hash % iphp->rrp.peers->number;                 /*根据hash值，得到被选中的后端服务器*/
 
         } else {
             w = hash % iphp->rrp.peers->total_weight;
@@ -238,7 +238,7 @@ ngx_http_upstream_get_ip_hash_peer(ngx_peer_connection_t *pc, void *data)
 
     iphp->rrp.current = p;
 
-    pc->sockaddr = peer->sockaddr;
+    pc->sockaddr = peer->sockaddr;   /*对nginx主动与后端服务器建立的连接赋值*/
     pc->socklen = peer->socklen;
     pc->name = &peer->name;
 
@@ -269,11 +269,11 @@ ngx_http_upstream_ip_hash(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 
     uscf->peer.init_upstream = ngx_http_upstream_init_ip_hash;  /*初始回调函数*/
 
-    uscf->flags = NGX_HTTP_UPSTREAM_CREATE  /*创建标志，如果含有创建标志的话，nginx会检查重复创建，以及必要参数是否填写*/
-                  |NGX_HTTP_UPSTREAM_WEIGHT     /*可以在server中使用weight属性*/
-                  |NGX_HTTP_UPSTREAM_MAX_FAILS  /*可以在server中使用max_fails属性*/
+    uscf->flags = NGX_HTTP_UPSTREAM_CREATE         /*创建标志，如果含有创建标志的话，nginx会检查重复创建，以及必要参数是否填写*/
+                  |NGX_HTTP_UPSTREAM_WEIGHT         /*可以在server中使用weight属性*/
+                  |NGX_HTTP_UPSTREAM_MAX_FAILS      /*可以在server中使用max_fails属性*/
                   |NGX_HTTP_UPSTREAM_FAIL_TIMEOUT   /*可以在server中使用fail_timeout属性*/
-                  |NGX_HTTP_UPSTREAM_DOWN;  /*可以在server中使用down属性*/
+                  |NGX_HTTP_UPSTREAM_DOWN;          /*可以在server中使用down属性*/
 
     return NGX_CONF_OK;
 }
